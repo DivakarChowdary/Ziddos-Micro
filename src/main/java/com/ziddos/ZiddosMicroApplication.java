8c4e159edf530c3bf6bc5bf14d79c9bd6796fdf1
diwchowdary@gmail.com <diwchowdary@gmail.com>
2021-12-20 22:22:26 -0600

    initial commit

---
+++ src/main/java/com/ziddos/ZiddosMicroApplication.java
@@ -0,0 +1,13 @@
+package com.ziddos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiddosMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZiddosMicroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ziddos/ZiddosMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziddos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiddosMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
